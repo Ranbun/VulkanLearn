1a7ea74afb3b994a63a697afa74384f4a07e8853
我来人间窜酒局 <granbao@gmail.com>
2022-11-30 23:16:44 +0800

    创建vkinstance

--- example/triangle/vulkanTriangle.cpp
+++ example/triangle/vulkanTriangle.cpp
@@ -1,23 +1,71 @@
 #include "vulkanTriangle.h"
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+
 void HelloTriangleApplication::run()
 {
-    initVulkan();
+    initWindow();
+    initVulKan();
     mainLoop();
     cleanup();
 }
 
-void HelloTriangleApplication::initVulkan()
+void HelloTriangleApplication::initWindow()
 {
+    glfwInit();
 
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); ///< 阻止GLFW创建上下文(OpenGL Context)
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); ///< 禁止resize
+
+    m_window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan Window", nullptr, nullptr);
 }
 
-void HelloTriangleApplication::mainLoop()
+void HelloTriangleApplication::initVulKan()
 {
+    createInstance();
+}
 
+void HelloTriangleApplication::mainLoop() const
+{
+    while (!glfwWindowShouldClose(m_window))
+    {
+        glfwPollEvents();
+    }
 }
 
 void HelloTriangleApplication::cleanup()
 {
+    glfwDestroyWindow(m_window);
+    glfwTerminate();
+    m_window = nullptr;
+}
+
+void HelloTriangleApplication::createInstance()
+{
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint32_t glfwExtensionCount = 0;
+
+    const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    createInfo.enabledExtensionCount = glfwExtensionCount; ///< 扩展数量 
+    createInfo.ppEnabledExtensionNames = glfwExtensions;  ///< 扩展名称
+    createInfo.enabledLayerCount = 0; ///< 全局校验层 
+
+    VkResult result = vkCreateInstance(&createInfo, nullptr, &m_vkInstance);
+
 
 }
--- example/triangle/vulkanTriangle.h
+++ example/triangle/vulkanTriangle.h
@@ -5,17 +5,36 @@
 #include <functional>
 #include <cstdlib>
 
+#define GLFW_INCLUDE_VULKAN
+
+class GLFWwindow;
+
 class HelloTriangleApplication
 {
 public:
     void run();
 
 private:
-    void initVulkan();
 
-    void mainLoop();
+    void initWindow();
+
+    void initVulKan();
+
+    void mainLoop() const;
 
     void cleanup();
+
+    void createInstance();
+
+
+private:
+
+    GLFWwindow* m_window{nullptr};
+    const int WIDTH = 800;
+    const int HEIGHT = 600;
+
+    /// VulKan
+    VkInstance m_vkInstance = nullptr;
 
 
 
