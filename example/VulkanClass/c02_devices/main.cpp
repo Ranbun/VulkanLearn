127cc350c025ff704816d87bf0c8cbdf682068f2
RBun <granbun@gmail.com>
2025-01-02 22:30:32 +0800

    create logoc device & check layer(instance level)

--- example/VulkanClass/c02_devices/main.cpp
+++ example/VulkanClass/c02_devices/main.cpp
@@ -15,6 +15,19 @@ int main(int argc, char ** args)
         return 1;
     }
 
+    uint32_t layerCount = 0;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> layers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, layers.data());
+
+    for(auto & layer: layers)
+    {
+        std::cout << "Layers Name: " << layer.layerName << std::endl;
+        std::cout << "Layers Des: " << layer.description << std::endl;
+    }
+
+
     /// select physical deveice
     uint32_t deviceCount = 0;
     VkResult reslut = vkEnumeratePhysicalDevices(app.getInstance(), &deviceCount, nullptr);
@@ -64,9 +77,44 @@ int main(int argc, char ** args)
     /// requirement phyiscal device  feature
     VkPhysicalDeviceFeatures physicalDeviceFeatures;
     vkGetPhysicalDeviceFeatures(masterPhysicalDevice, &physicalDeviceFeatures);
-
+    std::cout << "Geomtry: " << bool(physicalDeviceFeatures.geometryShader) << std::endl;
+    std::cout << "Tessellation: " << bool(physicalDeviceFeatures.tessellationShader) << std::endl;
 
     ///  create logsical device
+    VkDeviceQueueCreateInfo queueCreateInfo
+    {
+        VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+        nullptr,0,
+        0, 1, /// queueFamily, queueCount
+        nullptr
+    };
+
+    VkDeviceCreateInfo logicDeviceCreateInfo
+    {
+        VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+        nullptr, 0,
+        1, &queueCreateInfo,
+        0, nullptr,
+        0, nullptr,
+        &physicalDeviceFeatures
+    };
+
+    VkDevice logicDevice = nullptr;
+    reslut = vkCreateDevice(masterPhysicalDevice, &logicDeviceCreateInfo, nullptr, &logicDevice);
+    if(reslut != VK_SUCCESS)
+    {
+        std::cout<<"Failed to create logic Device"<<std::endl;
+        return 1;
+    }
+
+    std::cout<< logicDevice <<std::endl;
+    vkDestroyDevice(logicDevice, nullptr);
+
+    /// create swap chain
+
+
+
+
 
     return 0;
 }
