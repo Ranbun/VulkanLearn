5a72f8750c5e7b8bee077160c3e02c7aa1e25484
ranlh <granbao@gmail.com>
2025-11-08 23:05:25 +0800

    set clangd

--- build_cmake_debug_linux.sh
+++ build_cmake_debug_linux.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
-cmake -DCMAKE_BUILD_TYPE=Debug -G "Ninja" -DGLFW_INSTALL=OFF -S . -B ./out/build
+#! /bin/bash
+
+cmake -DCMAKE_BUILD_TYPE=Debug -G "Ninja" -DGLFW_INSTALL=OFF    -DCMAKE_TOOLCHAIN_FILE=/home/ranlh/develop/vcpkg/scripts/buildsystems/vcpkg.cmake  \
+                                                                -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -S . -B ./out/build
+
 ninja -C ./out/build/
