a3419b7d0e2b559df15adfb362953ac0ff0551a6
南无仙 <ranlonghui.person@gmail.com>
2025-12-05 21:23:18 +0800

    update: add case
    1. rendering triangle

---
+++ example/vulkan-tutorial/14_Rendering/Window.cpp
@@ -0,0 +1,55 @@
+#include "Window.h"
+#include <stdexcept>
+
+AppWindow::AppWindow(int width, int height, std::string &title) : m_window(nullptr)
+{
+    initWindow(width, height, title);
+};
+
+AppWindow::~AppWindow() { cleanup(); };
+
+void *AppWindow::getNativeWindow() { return reinterpret_cast<void *>(m_window); };
+
+bool AppWindow::shouldClose() { return glfwWindowShouldClose(m_window); };
+
+void AppWindow::pollEvent() { glfwPollEvents(); }
+
+void AppWindow::initWindow(int width, int height, std::string &title)
+{
+    if (!glfwInit())
+    {
+        throw std::runtime_error("Failed to initialize GLFW!");
+    }
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    m_window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
+    if (!m_window)
+    {
+        glfwTerminate();
+        throw std::runtime_error("Failed to create GLFW window!");
+    }
+};
+
+void AppWindow::cleanup()
+{
+    if (m_window)
+    {
+        glfwDestroyWindow(m_window);
+    }
+
+    glfwTerminate();
+};
+std::vector<const char *> AppWindow::getRequirementVulkanExtensions()
+{
+    /// 查询 glfw 需要依赖的实例扩展
+    uint32_t glfwExtensionCount = 0;
+    const char **glfwExtensions;
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+    if (!glfwExtensionCount)
+    {
+        return {};
+    }
+    std::vector<const char *> glfwRequirementExtensions{glfwExtensions, glfwExtensions + glfwExtensionCount};
+    return std::move(glfwRequirementExtensions);
+}
