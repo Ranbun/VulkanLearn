2fbead8ad3052feca95f6ee0565be99c609c2b8d
南无仙 <ranlonghui.person@gmail.com>
2025-12-15 20:54:56 +0800

    update: update case
    1. update case 17
    2. add stage buffer case

---
+++ example/vulkan-tutorial/VertexBuffer/18_StageBuffer/Application.h
@@ -0,0 +1,49 @@
+#ifndef APPLICATION_H
+#define APPLICATION_H
+
+#include <memory>
+#include "Window.h"
+
+#include "VulkanLayer.h"
+
+class Application
+{
+public:
+    Application(int width, int height, const std::string& title);
+    ~Application();
+
+    /**
+     * @brief Run the application main loop
+     */
+    void run() const;
+
+    void Init(int width, int height, const std::string& title);
+
+    AppWindow* renderWindow() const;
+
+    void OnEvent(Event& event);
+
+private:
+    void init(int width, int height, const std::string& title);
+    /**
+     * @brief Main application loop
+     */
+    void mainLoop() const;
+
+    /**
+     * @brief Cleanup Vulkan resources
+     */
+    void cleanup();
+
+    void drawFrame() const;
+
+private:
+    VulkanFeatureManager m_featureManager;
+    std::unique_ptr<AppWindow> m_window;
+    std::unique_ptr<VulkanLayer> m_vulkanLayer;
+
+    bool m_running{true};
+};
+
+
+#endif
---
+++ example/vulkan-tutorial/VertexBuffer/18_StageBuffer/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include "Application.h"
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        const Application app(800, 600, "StageBuffer");
+        app.run();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return 0;
+}
